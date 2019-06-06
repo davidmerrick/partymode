a4e905f41f848884f84a1d1e6e5e7af9ef683c62
David Merrick <david@david-merrick.com>
2019-06-05 17:41:51 -0700

    Not working, trying to get versions in an object

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,9 +6,9 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath(kotlin("gradle-plugin", version = "1.3.31"))
+        classpath(kotlin("gradle-plugin", version = Versions.kotlin))
         classpath("de.sebastianboegl.gradle.plugins:shadow-log4j-transformer:2.1.1")
-        classpath("com.github.jengelman.gradle.plugins:shadow:2.0.1")
+        classpath("com.github.jengelman.gradle.plugins:shadow:${Versions.shadow}")
     }
 }
 
@@ -19,8 +19,14 @@ repositories {
 
 plugins {
     java
-    kotlin("jvm") version "1.3.31"
-    id("com.github.johnrengelman.shadow") version "4.0.4"
+    `kotlin-dsl` // Enables experimental Kotlin compiler features
+    kotlin("jvm") version Versions.kotlin
+    id("com.github.johnrengelman.shadow") version Versions.shadow apply false
+}
+
+// Disable warnings about experimental features
+kotlinDslPluginOptions {
+    experimentalWarning.set(false)
 }
 
 dependencies {
@@ -30,16 +36,16 @@ dependencies {
     implementation("org.slf4j:slf4j-api:1.7.25")
     implementation("org.slf4j:slf4j-jdk14:1.7.26")
     implementation("io.github.microutils:kotlin-logging:1.6.10")
-    implementation("org.koin:koin-core:1.0.1")
+    implementation("org.koin:koin-core:${Versions.koin}")
     implementation("com.amazonaws:aws-lambda-java-core:1.1.0")
     implementation("com.amazonaws:aws-lambda-java-log4j2:1.0.0")
     implementation("com.amazonaws:aws-lambda-java-events:2.0.1")
-    implementation("com.amazonaws:aws-java-sdk-dynamodb:1.11.527")
-    implementation("com.amazonaws:aws-java-sdk-sns:1.11.527")
-    implementation("com.fasterxml.jackson.core:jackson-core:2.8.5")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.8.5")
-    implementation("com.fasterxml.jackson.core:jackson-annotations:2.8.5")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.8.5")
+    implementation("com.amazonaws:aws-java-sdk-dynamodb:${Versions.awsSdk}")
+    implementation("com.amazonaws:aws-java-sdk-sns:${Versions.awsSdk}")
+    implementation("com.fasterxml.jackson.core:jackson-core:${Versions.jackson}")
+    implementation("com.fasterxml.jackson.core:jackson-databind:${Versions.jackson}")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:${Versions.jackson}")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:${Versions.jackson}")
 
     testImplementation("org.testng:testng:6.14.3")
     testImplementation("org.koin:koin-test:1.0.1")
---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,7 @@
+object Versions {
+    const val kotlin = "1.3.31"
+    const val shadow = "4.0.4"
+    const val awsSdk = "1.11.527"
+    const val jackson = "2.8.5"
+    const val koin = "1.0.1"
+}

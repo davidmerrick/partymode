eebbe9e3c568dfd32b8a0089673cb9fb573475e2
David Merrick <david@david-merrick.com>
2019-06-14 15:59:26 -0700

    Added feature to reject call if origin is not from my number or call box

--- src/main/kotlin/com/merricklabs/partymode/handlers/CallHandlerLogic.kt
+++ src/main/kotlin/com/merricklabs/partymode/handlers/CallHandlerLogic.kt
@@ -10,9 +10,11 @@ import com.twilio.twiml.VoiceResponse
 import com.twilio.twiml.voice.Dial
 import com.twilio.twiml.voice.Number
 import com.twilio.twiml.voice.Play
+import com.twilio.twiml.voice.Reject
 import mu.KotlinLogging
 import org.koin.standalone.KoinComponent
 import org.koin.standalone.inject
+import java.net.URLDecoder
 
 private val log = KotlinLogging.logger {}
 
@@ -22,19 +24,41 @@ class CallHandlerLogic : RequestHandler<Map<String, Any>, ApiGatewayResponse>, K
     private val snsNotifier: SnsNotifier by inject()
 
     override fun handleRequest(input: Map<String, Any>, context: Context): ApiGatewayResponse {
-        val body = input["body"] as Map<String, Any>
+        val body = input["body"] as String
         log.info("Received input: $body")
-        log.info("From: ${body["From"]}")
 
-        // Twilio POSTS a application/x-www-form-urlencoded string to this endpoint
+        // Twilio POSTS an application/x-www-form-urlencoded string to this endpoint.
+        // Build a map with it.
+        val callParams = URLDecoder.decode(body, "UTF-8")
+                .split("&")
+                .map { it.split("=") }
+                .map { it[0] to it[1] }
+                .toMap()
 
+        callParams[FROM_FIELD]?.let {
+            if (it.contains(config.phone.callboxNumber) || it.contains(config.phone.myNumber)) {
+                log.info("Received a valid call from callbox or my number.")
+                return ApiGatewayResponse.build {
+                    rawBody = buildResponse().toXml()
+                    headers = mapOf("Content-Type" to "application/xml")
+                }
+            }
+        }
+
+        log.info("Invalid call. Rejecting.")
         return ApiGatewayResponse.build {
-            rawBody = getResponse().toXml()
+            rawBody = buildRejectResponse().toXml()
             headers = mapOf("Content-Type" to "application/xml")
         }
     }
 
-    private fun getResponse(): VoiceResponse {
+    private fun buildRejectResponse(): VoiceResponse {
+        return VoiceResponse.Builder()
+                .reject(Reject.Builder().build())
+                .build()
+    }
+
+    private fun buildResponse(): VoiceResponse {
         val partyLease = storage.getLatestItem()
         log.info("Got lease: $partyLease")
         if (partyLease.isActive()) {
@@ -56,6 +80,10 @@ class CallHandlerLogic : RequestHandler<Map<String, Any>, ApiGatewayResponse>, K
         if (config.sns.topicArn != null) {
             snsNotifier.notify(message)
         }
+    }
+
+    private companion object {
+        const val FROM_FIELD = "From"
     }
 }
 
--- src/test/kotlin/com/merricklabs/partymode/handlers/CallHandlerLogicTest.kt
+++ src/test/kotlin/com/merricklabs/partymode/handlers/CallHandlerLogicTest.kt
@@ -3,48 +3,55 @@ package com.merricklabs.partymode.handlers
 import com.amazonaws.services.lambda.runtime.Context
 import com.merricklabs.partymode.PartymodeIntegrationTestBase
 import com.merricklabs.partymode.models.PartyLease
-import com.merricklabs.partymode.testutil.MockPartymodeConfig
 import com.merricklabs.partymode.testutil.MockPartymodeConfig.Companion.CALLBOX_NUMBER
 import com.merricklabs.partymode.testutil.MockPartymodeConfig.Companion.MY_NUMBER
 import io.kotlintest.matchers.string.contain
 import io.kotlintest.shouldHave
+import io.kotlintest.shouldNotHave
 import org.koin.standalone.inject
 import org.mockito.Mockito
 import org.testng.annotations.Test
+import java.net.URLEncoder
 
 class CallHandlerLogicTest: PartymodeIntegrationTestBase() {
 
     private val callHandlerLogic: CallHandlerLogic by inject()
-    private val mockInput = mapOf("body" to mapOf("From" to CALLBOX_NUMBER))
+    private val mockInput = mapOf("body" to "From=${URLEncoder.encode(CALLBOX_NUMBER, "UTF-8")}")
     private val mockContext = Mockito.mock(Context::class.java)
 
     @Test
     fun `If partymode is enabled, should play digits`(){
-        val mockLease = Mockito.mock(PartyLease::class.java)
-        Mockito.`when`(mockLease.isActive()).thenReturn(true)
-        Mockito.`when`(partymodeStorage.getLatestItem()).thenReturn(mockLease)
+        initMockLease(true)
         val response = callHandlerLogic.handleRequest(mockInput, mockContext)
         response.body!!.toLowerCase() shouldHave contain("play digits")
     }
 
     @Test
     fun `If partymode is disabled, should forward to phone`(){
-        val mockLease = Mockito.mock(PartyLease::class.java)
-        Mockito.`when`(mockLease.isActive()).thenReturn(false)
-        Mockito.`when`(partymodeStorage.getLatestItem()).thenReturn(mockLease)
+        initMockLease(false)
         val response = callHandlerLogic.handleRequest(mockInput, mockContext)
         response.body!!.toLowerCase() shouldHave contain(MY_NUMBER)
     }
 
     @Test
     fun `If call is from callbox number, allow it`(){
-        val testPayload = mapOf(
-                "From" to CALLBOX_NUMBER
-        )
+        initMockLease(true)
+        val response = callHandlerLogic.handleRequest(mockInput, mockContext)
+        response.body!!.toLowerCase() shouldNotHave contain("reject")
     }
 
     @Test
     fun `If call is not from callbox number, reject it`(){
-        // Todo: Implement this
+        initMockLease(true)
+        val invalidNumber = "9999999999"
+        val mockInput = mapOf("body" to "From=${URLEncoder.encode(invalidNumber, "UTF-8")}")
+        val response = callHandlerLogic.handleRequest(mockInput, mockContext)
+        response.body!!.toLowerCase() shouldHave contain("reject")
+    }
+
+    private fun initMockLease(isActive: Boolean){
+        val mockLease = Mockito.mock(PartyLease::class.java)
+        Mockito.`when`(mockLease.isActive()).thenReturn(isActive)
+        Mockito.`when`(partymodeStorage.getLatestItem()).thenReturn(mockLease)
     }
 }

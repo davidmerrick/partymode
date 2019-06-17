2c0fe653c0807b4757e6fdf8b1bce494880a47bb
David Merrick <david@david-merrick.com>
2019-06-16 18:42:33 -0700

    Convert value

--- src/main/kotlin/com/merricklabs/partymode/handlers/SlackMessageHandlerLogic.kt
+++ src/main/kotlin/com/merricklabs/partymode/handlers/SlackMessageHandlerLogic.kt
@@ -4,6 +4,7 @@ import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.merricklabs.echobot.slack.SlackCallbackMessage
 import com.merricklabs.echobot.slack.SlackChallengeMessage
 import com.merricklabs.echobot.slack.SlackMessage
@@ -16,20 +17,21 @@ private val log = KotlinLogging.logger {}
 
 class SlackMessageHandlerLogic : RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent>, KoinComponent {
     private val bot: PartyBot by inject()
+    private val mapper: ObjectMapper by inject()
 
     override fun handleRequest(input: APIGatewayProxyRequestEvent, context: Context): APIGatewayProxyResponseEvent {
-        val message = input.body as SlackMessage
+        val message = mapper.convertValue(input.body, SlackMessage::class.java)
         return when (message.type) {
             "url_verification" -> {
                 log.info("Received challenge")
-                val challengeMessage = input.body as SlackChallengeMessage
+                val challengeMessage = mapper.convertValue(input.body, SlackChallengeMessage::class.java)
                 APIGatewayProxyResponseEvent().apply {
                     statusCode = 200
                     body = challengeMessage.challenge
                 }
             }
             "event_callback" -> {
-                val callbackMessage = input.body as SlackCallbackMessage
+                val callbackMessage = mapper.convertValue(input.body, SlackCallbackMessage::class.java)
                 bot.handle(callbackMessage)
                 APIGatewayProxyResponseEvent().apply {
                     statusCode = 200

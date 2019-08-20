f84dfa229d8cf32530085e1d64ea5e20b4e491d1
David Merrick <david@david-merrick.com>
2019-08-20 16:32:52 -0700

    Removed unused Slack webhook

--- src/main/kotlin/com/merricklabs/partymode/config/SlackConfig.kt
+++ src/main/kotlin/com/merricklabs/partymode/config/SlackConfig.kt
@@ -1,7 +1,6 @@
 package com.merricklabs.partymode.config
 
 class SlackConfig {
-    val webhookUri: String = System.getenv("SLACK_WEBHOOK_URI")
     val botToken: String = System.getenv("BOT_TOKEN")
     val botName = System.getenv("BOT_NAME") ?: "partybot"
 }

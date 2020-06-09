6450ca1401dda69b26d47b51edcfbf60509d706b
David Merrick <david@david-merrick.com>
2020-06-08 19:04:10 -0700

    Added back in validation

--- src/main/kotlin/io/github/davidmerrick/partymode/controllers/call/CallController.kt
+++ src/main/kotlin/io/github/davidmerrick/partymode/controllers/call/CallController.kt
@@ -3,30 +3,37 @@ package io.github.davidmerrick.partymode.controllers.call
 import io.github.davidmerrick.partymode.external.twilio.TwilioHeaders.TWILIO_SIGNATURE
 import io.github.davidmerrick.partymode.external.twilio.TwilioValidatorWrapper
 import io.micronaut.context.annotation.Context
+import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Header
 import io.micronaut.http.annotation.Post
-import java.net.URI
+import io.micronaut.http.server.util.HttpHostResolver
+import mu.KotlinLogging
+
+private val log = KotlinLogging.logger {}
 
 @Controller("/call")
 class CallController(
         private val logic: CallHandlerLogic,
-        private val validator: TwilioValidatorWrapper
+        private val validator: TwilioValidatorWrapper,
+        private val resolver: HttpHostResolver
 ) {
 
     @Post(consumes = [MediaType.APPLICATION_FORM_URLENCODED],
             produces = [MediaType.APPLICATION_XML])
     fun handleCall(
             @Header(TWILIO_SIGNATURE) twilioSignature: String,
-            @Context requestUrl: URI,
+            @Context request: HttpRequest<String>,
             @Body body: String
     ): HttpResponse<String> {
-//        if (!validator.validate(requestUrl.toString(), body, twilioSignature)) {
-//            return HttpResponse.badRequest("Failed to validate request")
-//        }
+        val requestUrl = resolver.resolve(request) + request.path
+        log.info("Received call request to $requestUrl")
+        if (!validator.validate(requestUrl, body, twilioSignature)) {
+            return HttpResponse.badRequest("Failed to validate request")
+        }
 
         val responseBody = logic.handleRequest(body)
         return HttpResponse.ok(responseBody)

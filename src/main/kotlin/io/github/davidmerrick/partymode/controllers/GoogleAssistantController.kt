d8d12c4c74dd6edbea878b9bf6f96fd4c8734499
David Merrick <david@david-merrick.com>
2020-11-04 14:43:11 -0800

    Revert "Revert "Upgrade to micronaut 2.0.0. Added tracing""
    This reverts commit 7e89f301f42c8cd128f0fb05557a8e115168b60e.

--- src/main/kotlin/io/github/davidmerrick/partymode/controllers/GoogleAssistantController.kt
+++ src/main/kotlin/io/github/davidmerrick/partymode/controllers/GoogleAssistantController.kt
@@ -8,6 +8,8 @@ import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
+import io.micronaut.security.annotation.Secured
+import io.micronaut.security.rules.SecurityRule.IS_AUTHENTICATED
 
 @Controller("/google")
 class GoogleAssistantController(
@@ -19,6 +21,7 @@ class GoogleAssistantController(
         consumes = [MediaType.APPLICATION_JSON],
         produces = [MediaType.APPLICATION_JSON]
     )
+    @Secured(IS_AUTHENTICATED)
     fun handleEvent(@Body eventString: String): String {
         val event = mapper.readValue<GoogleEvent>(eventString)
         return handler.handle(event) ?: ""

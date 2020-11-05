20604745b821c486a8b5ce08a4a6851fa2899cbc
David Merrick <david@david-merrick.com>
2020-11-04 21:45:11 -0800

    Check the google-assistant-signature value. Docs are incorrect in stating that the auth header is the one

--- src/main/kotlin/io/github/davidmerrick/partymode/controllers/GoogleAssistantController.kt
+++ src/main/kotlin/io/github/davidmerrick/partymode/controllers/GoogleAssistantController.kt
@@ -4,13 +4,17 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import io.github.davidmerrick.partymode.external.google.GoogleEvent
 import io.github.davidmerrick.partymode.external.google.GoogleEventHandler
 import io.github.davidmerrick.partymode.external.google.GoogleObjectMapper
-import io.micronaut.http.HttpHeaders
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Header
 import io.micronaut.http.annotation.Post
+import mu.KotlinLogging
+
+private val log = KotlinLogging.logger {}
+
+private const val GOOGLE_ASSISTANT_SIGNATURE = "google-assistant-signature"
 
 @Controller("/google")
 class GoogleAssistantController(
@@ -22,10 +26,11 @@ class GoogleAssistantController(
         produces = [MediaType.APPLICATION_JSON]
     )
     fun handleEvent(
-        @Header(HttpHeaders.AUTHORIZATION) authHeader: String?,
+        @Header(GOOGLE_ASSISTANT_SIGNATURE) authHeader: String?,
         @Body eventString: String
     ): HttpResponse<String> {
         // Todo: Add validation here
+        log.info(authHeader)
         authHeader ?: return HttpResponse.unauthorized()
 
         val event = mapper.readValue<GoogleEvent>(eventString)
